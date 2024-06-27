d9d67e32a47e3e12636c7fd1347b8790386b7304
OlegKuntyi <dr.o.kuntyi@gmail.com>
2024-06-27 08:38:23 +0200

    Update exam_explanations.js

--- src/constants/exam_explanations.js
+++ src/constants/exam_explanations.js
@@ -2864,7 +2864,7 @@ export const parentTabs = [
       },
       // 3tab
       {
-        id: 48,
+        id: 49,
         title: "Arzt-Arzt Gespräch",
         tableColumns: [
           { name: "first", visualText: "" },
@@ -2973,7 +2973,7 @@ export const parentTabs = [
       },
       // 4tab
       {
-        id: 49,
+        id: 50,
         title: "Die pathologische Befunden und Normalbefund",
         tableColumns: [
           { name: "first", visualText: "" },
@@ -3055,7 +3055,7 @@ export const parentTabs = [
       },
       // 5tab
       {
-        id: 50,
+        id: 51,
         title: "Gerätbeschreibung",
         img: child_tab_img_1,
         text: "Ein ERCP-Gerät ist ein spezialisiertes medizinisches Instrument, das zur endoskopischen retrograden Cholangiopankreatikographie (ERCP) verwendet wird. Es kombiniert endoskopische und radiologische Techniken, um detaillierte Bilder der Gallen- und Pankreasgänge zu erstellen und therapeutische Eingriffe durchzuführen. Hier sind die wesentlichen Komponenten und Funktionen eines ERCP-Geräts:Gantry: ist der ringförmige Teil des CT-Geräts, der die Röntgenröhre und die Detektoren enthält. Er kann sich um den Patienten drehen, um aus verschiedenen Winkeln Bilder zu erstellen.",
@@ -3139,7 +3139,7 @@ export const parentTabs = [
       },
       // 6tab
       {
-        id: 51,
+        id: 52,
         title: "Wortschatz",
         text: "Für eine Fachsprachenprüfung im Bereich der ERCP-Diagnostik sind folgende Begriffe und Phrasen wesentlich:",
         text_list: [
@@ -3302,7 +3302,7 @@ export const parentTabs = [
         ],
       },
       {
-        id: 52,
+        id: 53,
         title: "Links",
         text: "",
         text_list: [
@@ -3349,7 +3349,7 @@ export const parentTabs = [
         ],
       },
       {
-        id: 53,
+        id: 54,
         title: "PDF",
         link: "https://drive.google.com/file/d/1PqRkOW_XHA6f1_mxulrp3YX1hGNTV4uv/view?usp=share_link",
       },
