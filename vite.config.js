658dcc66c911d9cb84b0cd175304792ddb9fa86d
Michael Makaryshyn <mi5hqaaaaov@gmail.com>
2024-12-04 23:25:47 +0200

    Fix error

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import react from '@vitejs/plugin-react';
 
 export default {
   plugins: [react()],
-  build: {
-   rollupOptions: {
-     external: ['jspdf', 'react-firebase-hooks', 'jspdf-autotable', 'react-firebase-hooks/auth', 'firebase/firestore', 'firebase/auth', 'firebase/app'], // Exclude jsPDF from the bundle
-   },
- },
+//   build: {
+//    rollupOptions: {
+//      external: ['jspdf', 'react-firebase-hooks', 'jspdf-autotable', 'react-firebase-hooks/auth', 'firebase/firestore', 'firebase/auth', 'firebase/app'], // Exclude jsPDF from the bundle
+//    },
+//  },
 };
