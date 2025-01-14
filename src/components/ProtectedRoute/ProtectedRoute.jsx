cc2cc7bb61fb1605a21ba3cf3dc653641d7d94d1
OlegKuntyi <oleg.kuntyi@gmx.de>
2025-01-14 23:05:51 +0100

    “update”

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -8,7 +8,7 @@ const ProtectedRoute = ({ children }) => {
   const { currentUser } = useAuth();
 
   if (!currentUser) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/auth" replace />;
   }
 
   return children;
