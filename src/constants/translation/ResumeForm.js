771cffade6051841223050e0e1ac0b6301bf12f8
OlegKuntyi <oleg.kuntyi@gmx.de>
2025-02-01 16:52:09 +0100

    “update”

--- src/constants/translation/ResumeForm.js
+++ src/constants/translation/ResumeForm.js
@@ -28,35 +28,179 @@ const resumeFormTexts = {
     "Teilnahme an Workshops zur Optimierung der ärztlichen Dokumentation und Patientenkommunikation",
     "Übungen zur Verbesserung der ärztlichen Gesprächsführung in deutschen Kliniken und Praxen",
 ],
-  berufserfahrungenSuggestions: [
-    "Підказка ABeruf",
-    "Підказка B",
-  ],
-  ausbildungSuggestions: [
-    "Підказка Xосвіта",
-    "Підказка Y",
-  ],
-  languageSkillsSuggestions: [
-    "English",
-    "German",
-    "French",
-    "Spanish",
-  ],
-  levelSuggestions: [
-    "Beginner",
-    "Elementary",
-    "Intermediate",
-  ],
-  technicalSkillsSuggestions: [
-    "JavaScript",
-    "Python",
-    "Java",
-  ],
-  technicalLevelSuggestions: [
-    "Basic",
-    "Intermediate",
-    "Advanced",
-  ],
+berufserfahrungenSuggestions: [
+  "Hospitation in einer Klinik für [Fachrichtung] in Deutschland",
+  "Praktische Erfahrung durch eine Hospitation im Krankenhaus",
+  "Arbeit als Facharzt für [Fachrichtung] in einer Klinik oder Praxis",
+  "Assistenzarzt in einer Abteilung für [Fachrichtung] im Krankenhaus",
+  "Teilnahme an einer klinischen Weiterbildung für medizinische Fachkräfte",
+  "Gastarzt in einer Fachabteilung für [Fachrichtung] in einem deutschen Krankenhaus",
+  "Erste praktische Erfahrungen durch Famulaturen und Hospitationen in verschiedenen Fachrichtungen",
+  "Weiterbildung im Bereich [Fachrichtung] an einer Universitätsklinik",
+  "Klinische Tätigkeit in einer Abteilung für [Fachrichtung]",
+  "Praktische Erfahrung durch Arbeit in einer Poliklinik oder Praxis",
+  "Teilnahme an einem Intensivpraktikum in einer deutschen Klinik",
+  "Arbeit als Pflegekraft während des Medizinstudiums zur praktischen Erfahrungssammlung",
+  "Erfahrung in der Patientenbetreuung durch Tätigkeit in einer Notaufnahme oder Intensivstation",
+  "Hospitation in einer Abteilung für Innere Medizin oder Chirurgie zur Fachvertiefung",
+  "Mitarbeit in interdisziplinären Teams zur Diagnostik und Therapie von Patienten"
+],
+ausbildungSuggestions: [
+  "Studium der Humanmedizin an der Medizinischen Universität [Stadt, Land]",
+  "Abschluss des Humanmedizinstudiums an der Universität [Stadt, Land]",
+  "Medizinische Ausbildung an der Fakultät für Humanmedizin in [Stadt, Land]",
+  "Erfolgreicher Abschluss des Medizinstudiums an einer Universität in [Land]",
+  "Studium der Humanmedizin mit Schwerpunkt auf [Fachrichtung] an der Universität [Stadt]",
+  "Erwerb der Hochschulreife mit Schwerpunkt Naturwissenschaften in [Stadt, Land]",
+  "Abschluss der Schulausbildung mit dem Schwerpunkt Biologie und Chemie in [Stadt]",
+  "Voruniversitäre medizinische Ausbildung in [Stadt, Land]",
+  "Schulabschluss mit Spezialisierung auf medizinische Grundlagen in [Stadt]",
+  "Abschluss der allgemeinen Hochschulreife mit Schwerpunkt [Fachrichtung] in [Stadt]",
+  "Medizinstudium mit praxisorientierter Ausbildung in [Stadt, Land]",
+  "Klinische Ausbildung während des Medizinstudiums an der Universität [Stadt]",
+  "Praktische Ausbildung im Rahmen des Medizinstudiums an verschiedenen Kliniken in [Land]",
+  "Teilnahme an wissenschaftlichen Projekten während des Medizinstudiums an der Universität [Stadt]",
+  "Studium der Medizin mit Abschluss als Doktor der Humanmedizin in [Stadt, Land]"
+],
+languageSkillsSuggestions: [
+  "Deutsch",
+  "Englisch",
+  "Polnisch",
+  "Arabisch",
+  "Ukrainisch",
+  "Russisch",
+  "Französisch",
+  "Spanisch",
+  "Italienisch",
+  "Portugiesisch",
+  "Türkisch",
+  "Niederländisch",
+  "Rumänisch",
+  "Tschechisch",
+  "Ungarisch",
+  "Serbisch",
+  "Kroatisch",
+  "Bosnisch",
+  "Slowakisch",
+  "Slowenisch",
+  "Bulgarisch",
+  "Griechisch",
+  "Albanisch",
+  "Schwedisch",
+  "Dänisch",
+  "Norwegisch",
+  "Finnisch",
+  "Chinesisch",
+  "Japanisch",
+  "Koreanisch",
+  "Hindi",
+  "Bengalisch",
+  "Urdu",
+  "Tamil",
+  "Telugu",
+  "Marathi",
+  "Punjabi",
+  "Persisch",
+  "Paschtunisch",
+  "Kurdisch",
+  "Kasachisch",
+  "Aserbaidschanisch",
+  "Swahili",
+  "Hausa",
+  "Amharisch",
+  "Yoruba",
+  "Igbo",
+  "Somali",
+  "Afrikaans",
+  "Malagasy",
+  "Quechua",
+  "Guarani",
+  "Haitianisch-Kreolisch",
+  "Maori",
+  "Samoanisch",
+  "Hawaiianisch"
+],
+levelSuggestions: [
+  "Grundkenntnisse",          // Basic knowledge
+  "Elementare Kenntnisse",    // Elementary knowledge
+  "Mittelstufe",              // Intermediate
+  "Gute Kenntnisse",          // Good knowledge
+  "Fortgeschrittene Kenntnisse", // Advanced knowledge
+  "Sehr gute Kenntnisse",     // Very good knowledge
+  "Fließend",                 // Fluent
+  "Verhandlungssicher",       // Business proficient
+  "Muttersprache",            // Native language
+  "Zweitsprache"              // Second language
+],
+
+technicalSkillsSuggestions: [
+  // Інтегровані медичні та комп’ютерні навички
+  "Medizinische Dokumentation und Berichtserstellung",
+  "MS Office (Word, Excel, PowerPoint)",
+  "Elektronische Patientenakte (EPA) und Krankenhaus-Informationssysteme (KIS)",
+  "IT-Sicherheit und Datenschutz (DSGVO-konform)",
+  
+  // Діагностичні навички
+  "Ultraschalldiagnostik (Sonographie)",
+  "Abdominalsonographie",
+  "Herzsonographie (Echokardiographie)",
+  "Gefäßsonographie (Doppler- und Duplexsonographie)",
+  "Thoraxsonographie",
+  "Transkranielle Dopplersonographie",
+  "Muskuloskelettale Sonographie",
+  
+  // Радіологічні технології
+  "PACS-Systeme (Bildarchivierungs- und Kommunikationssystem)",
+  "Radiologische Bildbearbeitung (DICOM-Viewer)",
+  "CT- und MRT-Bildanalyse",
+  "Röntgendiagnostik (X-Ray Imaging)",
+  "Mammographie-Interpretation",
+  
+  // Лабораторні та клінічні навички
+  "Labordiagnostik-Software",
+  "Blutgasanalytik",
+  "Mikrobiologische und molekulare Diagnostik",
+  "Point-of-Care-Testing (POCT)",
+  "EKG-Analyse und Interpretation",
+  "Spirometrie und Lungenfunktionstests",
+  "Klinische Notfallsonographie (FAST-Protokoll)",
+  
+  // Загальні комп’ютерні навички
+  "Google Workspace (Docs, Sheets, Slides)",
+  "Projektmanagement-Tools (Trello, Asana, Jira)",
+  "Datenanalyse und Visualisierung (Power BI, Tableau)",
+  "Microsoft Teams",
+  "Grundlagen der Programmierung (JavaScript, Python, Java)",
+  "Zoom / Videokonferenz-Tools",
+  "Cloud-Services (AWS, Azure, Google Cloud)",
+  "Linux / MacOS / Windows Betriebssysteme",
+  "Datenbankverwaltung (SQL, Access)",
+  "HTML / CSS für Webanwendungen",
+  
+  // Додаткові клінічні навички
+  "Anwendung von medizinischen Leitlinien und Standards",
+  "Telemedizinische Plattformen",
+  "Medizinische Statistik (SPSS, R, Excel-Analyse)",
+  "Qualitätsmanagement im Gesundheitswesen",
+  "Arbeiten in interdisziplinären Teams",
+  "Klinische Forschung und wissenschaftliche Analyse"
+],
+technicalLevelSuggestions: [
+  "Grundkenntnisse",                 // Basic knowledge
+  "Basiswissen",                     // Foundational knowledge
+  "Gute Kenntnisse",                 // Good knowledge
+  "Fortgeschrittene Kenntnisse",     // Advanced knowledge
+  "Sehr gute Kenntnisse",            // Very good knowledge
+  "Expertenkenntnisse",              // Expert knowledge
+  "Sicherer Umgang mit [Tool/Technik]", // Confident handling
+  "Tägliche Anwendung in der Praxis",   // Daily practical application
+  "Fließende Anwendung im klinischen Alltag", // Fluent application in clinical practice
+  "Fundierte Erfahrung in [Bereich]",     // In-depth experience
+  "Spezialisierte Kenntnisse in [Fachgebiet]", // Specialized knowledge
+  "Professionelle Kompetenz",        // Professional competence
+  "Anwenderexperte",                 // Power user / Advanced user
+  "Trainer-Level"                    // Trainer level (ability to teach others)
+],
 };
 
 export default resumeFormTexts;
