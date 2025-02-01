11753948a2a57a3f819f7ff04f439e6fa6cb7c55
Michael Makaryshyn <mi5hqaaaaov@gmail.com>
2025-02-01 19:17:38 +0200

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
   build: {
-    outDir: 'dist', // Ensure output directory is explicitly set
-  },
+    outDir: 'public' 
+  }
 });
