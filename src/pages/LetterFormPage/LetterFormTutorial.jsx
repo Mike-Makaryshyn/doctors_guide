b8b8bdc920bd725290780cf729b04eb82f64d638
OlegKuntyi <oleg.kuntyi@gmx.de>
2025-03-09 18:30:41 +0100

    “update”

--- src/pages/LetterFormPage/LetterFormTutorial.jsx
+++ src/pages/LetterFormPage/LetterFormTutorial.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import Joyride, { STATUS } from "react-joyride";
 import tutorialTranslations from "./LetterFormTutorialTranslations";
 import useGetGlobalInfo from "../../hooks/useGetGlobalInfo";
+import { useMediaQuery } from 'react-responsive';
 import styles from "./LetterFormTutorial.module.scss";
 
 const LetterFormTutorial = ({ run, onFinish, openModal }) => {
@@ -11,74 +12,87 @@ const LetterFormTutorial = ({ run, onFinish, openModal }) => {
 
   const [steps, setSteps] = useState([]);
   const joyrideRef = useRef(null);
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
   useEffect(() => {
-    const stepsData = [
-      {
-        target: "body",
-        content: (
-          <span dangerouslySetInnerHTML={{ __html: translations.steps.intro }} />
-        ),
-        placement: "center",
-        disableBeacon: true,
-      },
-      {
-        target: '[data-tutorial="userAddressCard"]',
-        content: <span>{translations.steps.userAddressCard}</span>,
-        placement: "right",
-        disableBeacon: true,
-      },
-      {
-        target: '[data-tutorial="regionTile"]',
-        content: <span>{translations.steps.regionTile}</span>,
-        placement: "left",
-        disableBeacon: true,
-      },
-      {
-        target: '[data-tutorial="selectAddress"]',
-        content: <span>{translations.steps.selectAddress}</span>,
-        placement: "top",
-        disableBeacon: true,
-      },
-      {
-        target: '[data-tutorial="letterTextArea"]',
-        content: <span>{translations.steps.letterTextArea}</span>,
-        placement: "top",
-        disableBeacon: true,
-      },
-      {
-        target: '[data-tutorial="allInOneButton"]',
-        content: <span>{translations.steps.allInOneButton}</span>,
-        placement: "top",
-        disableBeacon: true,
-      },
-      {
-        target: '[data-tutorial="settingsButton"]',
-        content: <span>{translations.steps.settingsButton}</span>,
-        placement: "top",
-        disableBeacon: true,
-      },
-      {
-        target: '[data-tutorial="squareButton"]',
-        content: <span>{translations.steps.squareButton}</span>,
-        placement: "top",
-        disableBeacon: true,
-      },
-      {
-        target: '[data-tutorial="downloadButton"]',
-        content: <span>{translations.steps.downloadButton}</span>,
-        placement: "top",
-        disableBeacon: true,
-      },
-      {
-        target: '[data-tutorial="documentsButton"]',
-        content: <span>{translations.steps.documentsButton}</span>,
-        placement: "top",
-        disableBeacon: true,
-      },
-    ];
+    const stepsData = isMobile
+      ? [
+          {
+            target: "body",
+            content: (
+              <span dangerouslySetInnerHTML={{ __html: translations.steps.intro }} />
+            ),
+            placement: "center",
+            disableBeacon: true,
+          },
+        ]
+      : [
+          {
+            target: "body",
+            content: (
+              <span dangerouslySetInnerHTML={{ __html: translations.steps.intro }} />
+            ),
+            placement: "center",
+            disableBeacon: true,
+          },
+          {
+            target: '[data-tutorial="userAddressCard"]',
+            content: <span>{translations.steps.userAddressCard}</span>,
+            placement: "right",
+            disableBeacon: true,
+          },
+          {
+            target: '[data-tutorial="regionTile"]',
+            content: <span>{translations.steps.regionTile}</span>,
+            placement: "left",
+            disableBeacon: true,
+          },
+          {
+            target: '[data-tutorial="selectAddress"]',
+            content: <span>{translations.steps.selectAddress}</span>,
+            placement: "top",
+            disableBeacon: true,
+          },
+          {
+            target: '[data-tutorial="letterTextArea"]',
+            content: <span>{translations.steps.letterTextArea}</span>,
+            placement: "top",
+            disableBeacon: true,
+          },
+          {
+            target: '[data-tutorial="allInOneButton"]',
+            content: <span>{translations.steps.allInOneButton}</span>,
+            placement: "top",
+            disableBeacon: true,
+          },
+          {
+            target: '[data-tutorial="settingsButton"]',
+            content: <span>{translations.steps.settingsButton}</span>,
+            placement: "top",
+            disableBeacon: true,
+          },
+          {
+            target: '[data-tutorial="squareButton"]',
+            content: <span>{translations.steps.squareButton}</span>,
+            placement: "top",
+            disableBeacon: true,
+          },
+          {
+            target: '[data-tutorial="downloadButton"]',
+            content: <span>{translations.steps.downloadButton}</span>,
+            placement: "top",
+            disableBeacon: true,
+          },
+          {
+            target: '[data-tutorial="documentsButton"]',
+            content: <span>{translations.steps.documentsButton}</span>,
+            placement: "top",
+            disableBeacon: true,
+          },
+        ];
+
     setSteps(stepsData);
-  }, [translations]);
+  }, [translations, isMobile]);
 
   const handleJoyrideCallback = (data) => {
     const { status, index, action, type } = data;
