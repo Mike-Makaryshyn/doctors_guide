ab77d52e13acb1e6962f191aed7044798e2ecdc0
Michael Makaryshyn <mi5hqaaaaov@gmail.com>
2025-03-14 20:29:01 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(), svgr()],
-  build: {
-    outDir: 'dist',
-  },
-  assetsInclude: ['**/*.png', '**/*.PNG'] // Додаємо підтримку файлів з розширенням png та PNG
+   plugins: [react(), svgr()],
+   build: {
+      outDir: "dist",
+   },
+   optimizeDeps: {
+      include: ["zwitch"],
+   },
+   assetsInclude: ["**/*.png", "**/*.PNG"], // Додаємо підтримку файлів з розширенням png та PNG
 });
