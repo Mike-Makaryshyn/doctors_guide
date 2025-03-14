79a5c1efb6ead612b47feec9dcddf48eaad69865
Michael Makaryshyn <mi5hqaaaaov@gmail.com>
2025-03-14 20:37:44 +0200

    remove zwitch

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,5 @@ export default defineConfig({
    build: {
       outDir: "dist",
    },
-   optimizeDeps: {
-      include: ["zwitch"],
-   },
    assetsInclude: ["**/*.png", "**/*.PNG"], // Додаємо підтримку файлів з розширенням png та PNG
 });
