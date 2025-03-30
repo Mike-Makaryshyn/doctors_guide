19d510f8d6abdfed859ba55e4079a1d0214a3524
OlegKuntyi <oleg.kuntyi@gmx.de>
2025-03-30 08:43:20 +0200

    “update”

--- src/pages/MindMapPage/MindMapListView.jsx
+++ src/pages/MindMapPage/MindMapListView.jsx
@@ -1,39 +1,55 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import styles from './MindMapListView.module.scss';
 
 const MindMapListView = ({ data }) => {
-  // Стан для відстеження «натиснутих» плиток за їхнім ID
   const [pressedNodes, setPressedNodes] = useState({});
 
-  // Функція для перемикання стану конкретного вузла
   const togglePressed = (id) => {
     setPressedNodes((prev) => ({ ...prev, [id]: !prev[id] }));
   };
 
-  // Рекурсивна функція, яка відображає поточний вузол і його дітей
-  const renderList = (node) => (
-    <li key={node.id} className={styles.listItem}>
-      <div
-        className={`${styles.tile} ${
-          pressedNodes[node.id] ? styles.tilePressed : ''
-        }`}
-        onClick={() => togglePressed(node.id)}
+  const renderList = (node, index, siblings) => {
+    const isLastItem = index === siblings.length - 1;
+
+    return (
+      <li
+        key={node.id}
+        className={classNames(styles.listItem, {
+          [styles.lastItem]: isLastItem,
+        })}
       >
-        {node.label}
-      </div>
-      {node.children && node.children.length > 0 && (
-        <ul className={styles.nestedList}>
-          {node.children.map((child) => renderList(child))}
-        </ul>
-      )}
-    </li>
-  );
+        {/* Основний контейнер для плитки */}
+        <div className={styles.nodeContainer}>
+          <div className={styles.tileWrapper}>
+            <div
+              className={classNames(styles.tile, {
+                [styles.tilePressed]: pressedNodes[node.id],
+              })}
+              onClick={() => togglePressed(node.id)}
+            >
+              {node.label}
+            </div>
+          </div>
+
+          {/* Якщо є діти — обгортаємо їх у childrenContainer */}
+          {node.children && node.children.length > 0 && (
+            <div className={styles.childrenContainer}>
+              <ul className={styles.nestedList}>
+                {node.children.map((child, i) => renderList(child, i, node.children))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </li>
+    );
+  };
 
   return (
     <div className={styles.container}>
       <ul className={styles.rootList}>
-        {renderList(data)}
+        {renderList(data, 0, [data])}
       </ul>
     </div>
   );
