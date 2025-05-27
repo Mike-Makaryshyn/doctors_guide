988814cdfd81961baa18e001fe0d22d67f1becaf
OlegKuntyi <dr.o.kuntyi@gmail.com>
2025-05-27 21:22:14 +0200

    “update”

--- src/pages/AuthPage/CheckEmailPage.jsx
+++ src/pages/AuthPage/CheckEmailPage.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from './CheckEmailPage.module.css';
+import MainLayout from "../../layouts/MainLayout/MainLayout";
+import useGetGlobalInfo from "../../hooks/useGetGlobalInfo";
+import checkEmailTranslations from "../../constants/translation/checkEmail";
 
 export default function CheckEmailPage() {
+  const { selectedLanguage: language = "de" } = useGetGlobalInfo();
   return (
-    <div style={{ padding: "2rem" }}>
-      <h2>Fast fertig!</h2>
-      <p>
-        Wir haben dir eine E-Mail geschickt. Bitte öffne deine Mail und klicke den Bestätigungslink.
-      </p>
-      <p>
-        Hast du den Link schon geklickt? <Link to="/auth/confirm">Hier weiter</Link>.
-      </p>
-    </div>
+    <MainLayout>
+      <div className={styles.container}>
+        <h2 className={styles.heading}>
+          {checkEmailTranslations.heading[language]}
+        </h2>
+        <p className={styles.text}>
+          {checkEmailTranslations.description[language]}
+        </p>
+        <p className={styles.text}>
+          {checkEmailTranslations.prompt[language]}{" "}
+          <Link className={styles.link} to="/auth">
+            {checkEmailTranslations.linkText[language]}
+          </Link>
+          .
+        </p>
+      </div>
+    </MainLayout>
   );
 }
