f3a7b3fd4d9ed4c7c13fac08d83a0f88017ebbda
OlegKuntyi <dr.o.kuntyi@gmail.com>
2025-05-28 06:55:52 +0200

    Update cancelSubscription.js

--- src/constants/translation/cancelSubscription.js
+++ src/constants/translation/cancelSubscription.js
@@ -2,27 +2,27 @@ export default {
   message: {
     de: 'Der Vorgang Ihrer Subscription wurde abgebrochen.',
     en: 'Your subscription process was cancelled.',
-    fr: 'Votre processus d’abonnement a été annulé.',
-    ua: 'Процес підписки було скасовано.',
+    uk: 'Процес підписки було скасовано.',
     ru: 'Процесс подписки был отменён.',
-    tr: 'Abonelik süreciniz iptal edildi.',
+    tr: 'Abonelik işleminiz iptal edildi.',
     ar: 'تم إلغاء عملية الاشتراك الخاصة بك.',
-    es: 'Su proceso de suscripción ha sido cancelado.',
+    fr: 'Votre processus d’abonnement a été annulé.',
+    es: 'El proceso de suscripción ha sido cancelado.',
     pl: 'Proces subskrypcji został anulowany.',
-    el: 'Η διαδικασία συνδρομής σας ακυρώθηκε.',
-    ro: 'Procesul de abonare a fost anulat.'
+    el: 'Η διαδικασία συνδρομής ακυρώθηκε.',
+    ro: 'Procesul de abonament a fost anulat.',
   },
   retryLink: {
     de: 'Erneut versuchen',
     en: 'Try again',
-    fr: 'Réessayer',
-    ua: 'Спробувати знову',
-    ru: 'Попробовать ещё раз',
+    uk: 'Спробувати знову',
+    ru: 'Попробовать снова',
     tr: 'Tekrar deneyin',
     ar: 'حاول مرة أخرى',
+    fr: 'Réessayer',
     es: 'Intentar de nuevo',
     pl: 'Spróbuj ponownie',
     el: 'Δοκιμάστε ξανά',
-    ro: 'Încercați din nou'
+    ro: 'Încearcă din nou',
   }
 };
